d8f21fc4bd63924b3ff4d7495205ceaeac267975
LieryangStack <sxlisibo@gmail.com>
2023-11-04 17:14:20 +0800

    update

--- gtk4/example1/pr4.c
+++ gtk4/example1/pr4.c
@@ -1,4 +1,6 @@
 #include <gtk/gtk.h>
+#include <gdk/x11/gdkx.h>
+#include <X11/X.h>
 
 static void 
 app_activate (GApplication *app, gpointer *user_data) {
@@ -7,6 +9,20 @@ app_activate (GApplication *app, gpointer *user_data) {
   gtk_window_set_title (GTK_WINDOW (win), "pr4");
   gtk_window_set_default_size (GTK_WINDOW (win), 500, 400);
   gtk_window_present (GTK_WINDOW (win));
+
+  GtkWidget *btn;
+  btn = gtk_button_new_with_label ("Test Button");
+
+  GtkWidget *img;
+  img = gtk_image_new_from_file ("/home/lieryang/Pictures/lieryang.jpg");
+
+  gtk_window_set_child (GTK_WINDOW(win), img);
+
+  XID win_id = GDK_SURFACE_XID (gtk_native_get_surface(gtk_widget_get_native(win)));
+  XID btn_id = GDK_SURFACE_XID (gtk_native_get_surface(gtk_widget_get_native(img)));
+
+  g_print ("win_id = 0x%lx\n", win_id);
+  g_print ("btn_id = 0x%lx\n", btn_id);
 }
 
 
